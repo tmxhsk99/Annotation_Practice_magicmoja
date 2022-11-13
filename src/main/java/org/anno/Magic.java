e8fc90dd1674e7baa301d5597064eebc47556cd8
tmxhsk99 <tmxhsk99@naver.com>
2022-11-13 17:38:32 +0900

    feat : 자체 애너테이션 프로세서 기능추가
    - interface 만 적용가능한 애너테이션으로 만들도록 기능 추가
    - autoService 라이브러리 이용하여 빌드 시 등록한 애너테이션이 없는경우 먼저 컴파일 후 등록파일 resources/meta-inf/services/...을 생성하도록 처리함

--- src/main/java/org/anno/Magic.java
+++ src/main/java/org/anno/Magic.java
@@ -1,9 +1,16 @@
 package org.anno;
 
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+
+@Target(ElementType.TYPE) //애노테이션을 적용시킬 클래스를 한정시킬 수 있음 (Interface, Class, Enum)
 //해당 애노테이셔능 어디까지 유지할 건지 (소스,클래스,런타임)
 @Retention(RetentionPolicy.SOURCE)
+
 public @interface Magic {
 }
--- src/main/java/org/anno/MagicMojaProcessor.java
+++ src/main/java/org/anno/MagicMojaProcessor.java
@@ -1,11 +1,21 @@
 package org.anno;
 
+import com.google.auto.service.AutoService;
+
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
 import java.util.Set;
 
+@AutoService(Processor.class)
+@SupportedSourceVersion(SourceVersion.RELEASE_11)
 public class MagicMojaProcessor extends AbstractProcessor {
 
     //이 프로세서가 어떤 애노테이션을 처리할 것인가 정도는 오버라이드 해준다.
@@ -21,7 +31,22 @@ public class MagicMojaProcessor extends AbstractProcessor {
 
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         //여기서 리턴을 true를 리턴하면 해당 애너테이션 프로세서는 처리했다는 의미이므로 다른 프로세서들이 처리하지 않게된다.
-        //여기서는 처리하고 true를 리턴한다. 어떤경우는 여기서 도 처리하고 , 다른 프로세서에게 마지막으로 처리할 수도 있다.
+        //여기서는 처리하고 true를 리턴한다. 어떤경우는 여기서도 처리하고 , 다른 프로세서에게 마지막으로 처리할 수도 있다.
+
+        // Magic이라는 애노테이션이 붙은 앨리맨트를 전부 가져온다.
+        Set<? extends Element> elementsAnnotatedWith = roundEnv.getElementsAnnotatedWith(Magic.class);
+
+        //만약에 해당 Magic 이란 애노테이션이 interface에만 붙이도록 하고 싶은 경우
+        //애너테이션 자체의 @Target(ElementType.TYPE) 기능은 interface,Enum,Class 로 한정시키지 못하므로
+        //프로세서에서 자체적으로 걸러야한다.
+        for (Element element : elementsAnnotatedWith) {
+            Name simpleName = element.getSimpleName();
+            if (element.getKind() != ElementKind.INTERFACE) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "Magic annotation can not be used on " + simpleName);
+            } else {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,"Processing " + simpleName);
+            }
+        }
         return true;
     }
 }
